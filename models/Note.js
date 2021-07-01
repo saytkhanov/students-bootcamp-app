6cdafb7a0bf1fa22c5524b663d91cbca181f31de
saytkhanov <saythanov2014@mail.ru>
2021-07-01 19:30:30 +0300

    add model Note

---
+++ models/Note.js
@@ -0,0 +1,18 @@
+const {Schema, model, Types } = require('mongoose');
+
+
+const noteSchema = new Schema({
+    text: {
+      type: String
+    },
+    userStatus: {
+      type: Types.ObjectId,
+      ref: 'Student'
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Note = model('Note', noteSchema);
