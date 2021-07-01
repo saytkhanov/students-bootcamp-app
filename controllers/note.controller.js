6ed3bc7b390e4b7d5f9e1ae4e01f1ff4e3060fb6
saytkhanov <saythanov2014@mail.ru>
2021-07-02 01:59:18 +0300

    add controller for getAllNote for patching and delete also i modify other controllers

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -2,15 +2,50 @@ const Note = require('../models/Note');
 
 const controllers = {
   addNote: async (req, res) => {
-    const addNote = await new Note({
-       text: req.body.text,
-       userId: req.params.id
-    })
-    await addNote.save();
-    res.status(201).json({ message: "Комментарий добавлен" })
+    try {
+      const addNote = await new Note({
+        text: req.body.text,
+        userId: req.params.id
+      })
+      await addNote.save();
+      res.status(201).json({ message: "Комментарий добавлен" })
+    } catch (e) {
+      console.log(e.message)
+    }
   },
   getNoteById: async (req, res) => {
-    const getNoteById = await Note.findById(req.params.id)
-    res.status(201).json({ message: "Комментарии" })
+    try {
+      const getNoteById = await Note.findById(req.params.id)
+      res.status(201).json({ message: "Комментарии" })
+    } catch (e) {
+      console.log(e.message)
+    }
   },
+  getAllNote: async (req, res) => {
+   try {
+     const getAllNotes = await Note.find();
+     res.status(201).json({ message: "Комментарии получены" })
+   } catch (e) {
+     console.log(e.message)
+   }
+  },
+  patchNote: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const {text} = req.body;
+      const options = {new: true}
+      const patchNote = await Note.findByIdAndUpdate(id, {text},options);
+      res.status(201).json({ message: "Комментарий изменен" })
+    } catch (e) {
+      console.log(e.message)
+    }
+  },
+  deleteNote: async (req, res) => {
+    try {
+      const deleteNote = await Note.findByIdAndDelete(req.params.id)
+      res.status(201).json({ message: "Комментарий удален" })
+    } catch (e) {
+      console.log(e.message)
+    }
+  }
 }
--- controllers/status.controller.js
+++ controllers/status.controller.js
@@ -2,19 +2,27 @@ const Status = require('../models/Status');
 
 const controllers = {
   addStatus: async (req, res) => {
-    const addStatus = await new Status({
-      type: req.body.type,
-      userId: req.params.id
-    })
-    await addStatus.save()
-    res.status(201).json({ message: "Cтатус добавлен" })
+ try {
+   const addStatus = await new Status({
+     type: req.body.type,
+     userId: req.params.id
+   })
+   await addStatus.save()
+   res.status(201).json({ message: "Cтатус добавлен" })
+ } catch (e) {
+   console.log(e.message)
+ }
   },
   patchStatus: async (req, res) => {
-    const id = req.params.id;
-    const {type} = req.body.type;
-    const options = {new: true}
-    const patchStatus = await Status.findByIdAndUpdate(id, {type}, options)
-    await patchStatus.save();
-    res.status(201).json({ message: "Cтатус изменен" })
+   try {
+     const id = req.params.id;
+     const {type} = req.body.type;
+     const options = {new: true}
+     const patchStatus = await Status.findByIdAndUpdate(id, {type}, options)
+     await patchStatus.save();
+     res.status(201).json({ message: "Cтатус изменен" })
+   } catch (e) {
+     console.log(e.message)
+   }
   }
 }
