f5baeef4d978b08376cb1ba58477d39698c02de3
saytkhanov <81220037+saytkhanov@users.noreply.github.com>
2021-07-28 21:01:46 +0300

    деструктуризировал port

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
-const { PORT } = process.env.PORT
+const { PORT } = process.env.PORT || 4004
 
 const start = async () => {
   try {
