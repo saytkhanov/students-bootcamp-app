6c82a0fcdcf533407b9873be770b12dbfdef6219
saytkhanov <81220037+saytkhanov@users.noreply.github.com>
2021-07-28 20:45:42 +0300

    опять же исправление недоработок

--- index.js
+++ index.js
@@ -9,16 +9,19 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
 
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.resolve(__dirname, 'client', 'build')))
 app.use(routes);
 app.use(morgan("combined"));
+app.use(express.static(path.resolve(__dirname, 'client', 'build')))
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
+
+const { PORT } = process.env.PORT
 
 const start = async () => {
   try {
@@ -27,8 +30,8 @@ const start = async () => {
       useFindAndModify: false,
       useNewUrlParser: true,
     });
-    app.listen(process.env.PORT, () => {
-      console.log(`Server has been started on port: ${process.env.PORT}...`);
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port: ${PORT}...`);
     });
   } catch (e) {
     console.log(e.message);
