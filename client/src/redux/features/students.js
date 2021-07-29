dd1772ada8e88abda80d83137c0335ae695773e9
saytkhanov <81220037+saytkhanov@users.noreply.github.com>
2021-07-29 11:29:26 +0300

    изменил роуты

--- client/src/redux/features/students.js
+++ client/src/redux/features/students.js
@@ -70,7 +70,7 @@ export const loadStudents = () => {
 export const createStudent = (data) => {
   return async (dispatch) => {
     dispatch({ type: "students/create/pending" });
-    const response = await fetch("https://localhost:3004/student", {
+    const response = await fetch("/student", {
       method: "POST",
       headers: {
         "Content-type": "application/json",
